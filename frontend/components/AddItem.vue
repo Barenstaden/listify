660578fb46a1e57a49278d6810cbc33c19cbf44e
Thomas Bjørnstad <barenstaden@yahoo.no>
2021-10-18 19:24:23 +0200

    Final fiks?

--- frontend/components/AddItem.vue
+++ frontend/components/AddItem.vue
@@ -42,7 +42,9 @@ export default {
     notFound: false,
     updateCategory: false,
     client: new MeiliSearch({
-      host: "http://localhost:3001/search/",
+      host: window.location.origin
+        ? window.location.origin + "/search/"
+        : "/search/",
       apiKey: "266e2d0f7855516b3bb47172c89c80b639f8e90cbe932b16d1e0dd7267754537"
     })
   }),
