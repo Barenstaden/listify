93a0dafbdbd04061aeaa4fdcae5037cf374a68a6
Thomas Bjørnstad <barenstaden@yahoo.no>
2021-10-29 23:00:36 +0200

    Build test

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -132,8 +132,8 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     // analyze: true,
-    parallel: true,
-    hardSource: true,
+    // parallel: true,
+    // hardSource: true,
     cache: true
     // babel: {
     //   plugins: [
