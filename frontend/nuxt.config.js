8fc57f895f650060708af7349fe9ed14c17f8284
Thomas Bjørnstad <barenstaden@yahoo.no>
2021-11-01 14:23:53 +0100

    Build cache

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -147,7 +147,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   //
   build: {
-    // cache: true
+    cache: true
     // babel: {
     //   plugins: [
     //     ["@babel/plugin-proposal-private-methods", { loose: true }],
